e50f4d32614c315276f16d6f3d620986f490ce42
Samuel Yamoah <sya57@uclive.ac.nz>
2016-05-28 19:44:03 +1200

    Added a working Serial Coms and Buttons. NOTE: PID and Sensors will need to be added.

--- motor_pwmGen.c
+++ motor_pwmGen.c
@@ -18,8 +18,12 @@
 #include "driverlib/systick.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/interrupt.h"
+#include "driverlib/uart.h"
 #include "drivers/rit128x96x4.h"
+#include "utils/uartstdio.h"
 #include "utils/ustdlib.h"
+#include "stdio.h"
+#include "stdlib.h"
 #include "buttonSet.h"
 #include "button.h"
 
@@ -33,8 +37,16 @@
 #define MOTOR_RATE_HZ 200
 #define PWM_DIV_CODE SYSCTL_PWMDIV_4
 #define PWM_DIVIDER 4
-#define MOTOR_DUTY_MAIN 80
-#define MOTOR_DUTY_TAIL 2
+#define MOTOR_DUTY_MAIN 10
+#define MOTOR_DUTY_TAIL 10
+
+#define BAUD_RATE 9600ul
+
+signed int main_duty = 0;
+signed int tail_duty = 0;
+int state = 0;
+static volatile unsigned long g_tickCnt;
+unsigned long period;
 
 //*******************************************************************
 // ISR for the SysTick interrupt (used for button debouncing).
@@ -45,6 +57,9 @@ SysTickIntHandler (void)
 	//
 	// Poll the buttons
 	updateButtons();
+
+	//Polls display on UART0
+	g_tickCnt++;
 }
 
 //*******************************************************************
@@ -76,6 +91,148 @@ initSysTick (void)
     // Enable interrupt and device
     SysTickIntEnable ();
     SysTickEnable ();
+}
+
+void ButtPressIntHandler (void)
+{
+	unsigned long ulSelect; //SELECT
+	unsigned long ulUp; //UP
+	unsigned long ulDown; //DOWN
+	unsigned long ulCCw; //CCW/RIGHT
+	unsigned long ulCw; //CW/LEFT
+	unsigned long ulReset; //RESET
+	//int power = 0;
+
+	// Clear the interrupt (documentation recommends doing this early)
+	GPIOPinIntClear (GPIO_PORTB_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6);
+
+	// Read the pins simultaneously to ensure the right states are read
+	ulReset = GPIOPinRead (GPIO_PORTB_BASE, GPIO_PIN_1);
+	ulCw = GPIOPinRead (GPIO_PORTB_BASE, GPIO_PIN_2);
+	ulCCw = GPIOPinRead (GPIO_PORTB_BASE, GPIO_PIN_3);
+	ulSelect = GPIOPinRead (GPIO_PORTB_BASE, GPIO_PIN_4);
+	ulUp = GPIOPinRead (GPIO_PORTB_BASE, GPIO_PIN_5);
+	ulDown = GPIOPinRead (GPIO_PORTB_BASE, GPIO_PIN_6);
+
+	// Compute the PWM period in terms of the PWM clock
+	    period = SysCtlClockGet () / PWM_DIVIDER / MOTOR_RATE_HZ;
+
+	if(ulUp == 0 && main_duty < 98){
+		main_duty += 10;
+		if (main_duty >= 98) main_duty = 98;
+		PWMPulseWidthSet (PWM_BASE, PWM_OUT_1, period * main_duty /100);
+	}
+
+	if(ulDown == 0 && main_duty > 10){
+		main_duty -= 10;
+		if (main_duty <= 10) main_duty = 10;
+		PWMPulseWidthSet (PWM_BASE, PWM_OUT_1, period * main_duty /100);
+		}
+
+	if(ulCCw == 0 && tail_duty < 98){
+		tail_duty += 15;
+		if (tail_duty >= 98) tail_duty = 98;
+		PWMPulseWidthSet (PWM_BASE, PWM_OUT_4, period * tail_duty /100);
+		}
+
+	if(ulCw == 0 && tail_duty > 10){
+		tail_duty -= 15;
+		if (tail_duty <= 10) tail_duty = 10;
+		PWMPulseWidthSet (PWM_BASE, PWM_OUT_4, period * tail_duty /100);
+		}
+
+	if(ulSelect == 0){
+		if (state == 0){
+			main_duty = MOTOR_DUTY_MAIN;
+			tail_duty = MOTOR_DUTY_TAIL;
+			state = 1;
+		}
+		//if (state == 1){
+			//main_duty = 0;
+			//tail_duty = 0;
+			//state = 0;
+
+		//}
+
+	if(ulReset == 0){
+		if (!ulReset) SysCtlReset();
+		}
+}
+}
+
+void intButton (void)
+{
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+	// Register the handler for Port B into the vector table
+	GPIOPortIntRegister (GPIO_PORTB_BASE, ButtPressIntHandler);
+
+	//Initialising for buttons
+	GPIOPinTypeGPIOInput (GPIO_PORTB_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6);
+	GPIOPadConfigSet (GPIO_PORTB_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+	GPIOIntTypeSet (GPIO_PORTB_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6, GPIO_FALLING_EDGE);
+	GPIOPinIntEnable (GPIO_PORTB_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6);
+
+}
+
+//**********************************************************************
+// Initialise UART0 - 8 bits, 1 stop bit, no parity
+//**********************************************************************
+initConsole (void)
+{
+    //
+    // Enable GPIO port A which is used for UART0 pins.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    //
+    // Select the alternate (UART) function for these pins.
+    //
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), BAUD_RATE,
+                        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                         UART_CONFIG_PAR_NONE));
+    UARTFIFOEnable(UART0_BASE);
+    UARTEnable(UART0_BASE);
+}
+
+//**********************************************************************
+// Transmit a string via UART0
+//**********************************************************************
+void
+UARTSend (char *pucBuffer)
+{
+    //
+    // Loop while there are more characters to send.
+    //
+    while(*pucBuffer)
+    {
+        //
+        // Write the next character to the UART Tx FIFO.
+        //
+        UARTCharPut(UART0_BASE, *pucBuffer);
+        pucBuffer++;
+    }
+}
+
+//**********************************************************************
+// Transmit a dummy "status" block of helicopter data, with buffering
+//**********************************************************************
+void
+TxStatus (void)
+{
+	char string[40];
+
+	//sprintf(string, "butt = %d  ", button);
+	//RIT128x96x4StringDraw(string, 5, 14, 15);
+
+	if ((g_tickCnt % 25) == 0){
+
+
+		sprintf(string, " Main Duty: %d\n----------\n", main_duty);
+		UARTSend (string);
+		sprintf(string, " Tail Duty: %d\n----------\n", tail_duty);
+		UARTSend (string);
+   }
 }
 
 //******************************************************************
@@ -149,13 +306,14 @@ displayInfo(int main, int tail)
 int
 main (void)
 {
-    signed int main_duty = MOTOR_DUTY_MAIN;
-    signed int tail_duty = MOTOR_DUTY_TAIL;
-    unsigned long period;
+
+
 
     initClock ();
     initPin ();
     initDisplay ();
+    intButton();
+	initConsole ();
     initButSet (UP_B | DOWN_B | LEFT_B | RIGHT_B, SYSTICK_RATE_HZ);
     initPWMchan ();
     initSysTick ();
@@ -164,15 +322,14 @@ main (void)
     // Enable interrupts to the processor.
     IntMasterEnable();
     //
-    // Compute the PWM period in terms of the PWM clock
-    period = SysCtlClockGet () / PWM_DIVIDER / MOTOR_RATE_HZ;
+
     //
     // Loop forever while controlling the PWM duty cycle.
     while (1)
     {
        // Background task: Check for button pushes and control the
        //  PWM duty cycle +/- 5% within the range 2% to 98%.
-       if (checkBut (UP) && main_duty < 98)
+      /* if (checkBut (UP) && main_duty < 98)
        {
     	  main_duty += 10;
     	  if (main_duty >= 98) main_duty = 98;
@@ -197,9 +354,10 @@ main (void)
     	  tail_duty -= 15;
     	  if (tail_duty <= 2) tail_duty = 2;
           PWMPulseWidthSet (PWM_BASE, PWM_OUT_4, period * tail_duty /100);
-       }
+       }*/
 
        displayInfo(main_duty, tail_duty);
+       TxStatus ();
 
     }
 }
