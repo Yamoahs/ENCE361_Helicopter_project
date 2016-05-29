2b8920545796fdce7e4046d70b8b97e827d189a5
Samuel Yamoah <sya57@uclive.ac.nz>
2016-05-30 11:59:39 +1200

    Added a title block lol.

--- pid_control.c
+++ pid_control.c
@@ -1,2 +1,10 @@
-//*****************************************************
-//pid_control.c
+//************************************************************************
+//
+//pid_control.c - A PID Control system that will control the duty cycle of
+//				  the Tail and Main Motor Output
+//
+// Author:  Samuel Yamoah
+// Date Created: 25.5.2016
+// Last modified: 30.5.2016
+//
+//************************************************************************
