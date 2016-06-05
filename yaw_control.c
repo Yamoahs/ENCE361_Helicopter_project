79c2330f3779e41a9fa1071c616d8fdbe076ba82
Samuel Yamoah <sya57@uclive.ac.nz>
2016-06-05 21:12:58 +1200

    Finished the Yaw Module

--- yaw_control.c
+++ yaw_control.c
@@ -6,6 +6,26 @@
 @brief		 A Yaw Control system that will control the yaw of the helicopter via
            the Tail Motor
 *******************************************************************************/
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/adc.h"
+#include "driverlib/pwm.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/debug.h"
+#include "driverlib/uart.h"
+#include "drivers/rit128x96x4.h"
+#include "utils/uartstdio.h"
+#include "utils/ustdlib.h"
+#include "utils/isqrt.h"
+#include "utils/circBuf.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "yaw_control.h"
+
 
 //******************************************************************************
 // Determines the yaw movement based on state changes. If the state change
@@ -13,7 +33,7 @@
 // is increased. If the currentState is the same as the previousState then no
 // change in yaw else yaw is decreased.
 //******************************************************************************
-void yawCalc (void)
+void yawCalc (int previousState, int currentState, int yaw)
 {
 	if (previousState == 1 && currentState == 2 ||
       previousState == 2 && currentState == 3	||
@@ -32,81 +52,13 @@ void yawCalc (void)
 	}
 }
 
-//******************************************************************************
-// The interrupt handler for the for the pin change interrupt. Note that
-//  the SysTick counter is decrementing.
-//******************************************************************************
-void YawChangeIntHandler (void)
-{
-	/*States:
-	 * A 1 = 00
-	 * B 2 = 01
-	 * C 3 = 11
-	 * D 4 = 10
-	 */
-
-	unsigned long ulPortValA;
-	unsigned long ulPortValB;
-
-	// Clear the interrupt (documentation recommends doing this early)
-	GPIOPinIntClear (GPIO_PORTF_BASE, GPIO_PIN_5 |  GPIO_PIN_7);
-
-	// Read the pin
-	ulPortValA = GPIOPinRead (GPIO_PORTF_BASE, GPIO_PIN_5);
-	ulPortValB = GPIOPinRead (GPIO_PORTF_BASE, GPIO_PIN_7);
-	previousState = currentState;
-
-	if (!ulPortValA)
-	{
-		if (ulPortValB)
-		{
-			currentState = 2;
-		}
-		else
-		{
-			currentState = 1;
-		}
-	}
-	else
-	{
-		if (ulPortValB)
-		{
-			currentState = 3;
-		}
-		else
-		{
-			currentState = 4;
-		}
-	}
-
-	yawCalc();
-}
-
-// *****************************************************************************
-void initYaw (void)
-{
-    // Register the handler for Port F into the vector table
-    GPIOPortIntRegister (GPIO_PORTF_BASE, YawChangeIntHandler);
-
-    // Enable and configure the port and pin used:  input on PF5: Pin 27 & PF7: Pin 29
-    SysCtlPeripheralEnable (SYSCTL_PERIPH_GPIOF);
-    GPIOPadConfigSet (GPIO_PORTF_BASE, GPIO_PIN_5 | GPIO_PIN_7, GPIO_STRENGTH_2MA,
-                      GPIO_PIN_TYPE_STD_WPU);
-
-    // Set up the pin change interrupt (both edges)
-    GPIOIntTypeSet (GPIO_PORTF_BASE, GPIO_PIN_5 | GPIO_PIN_7, GPIO_BOTH_EDGES);
-
-    // Enable the pin change interrupt
-    GPIOPinIntEnable (GPIO_PORTF_BASE, GPIO_PIN_5 | GPIO_PIN_7);
-    IntEnable (INT_GPIOF);	// Note: INT_GPIOF defined in inc/hw_ints.h
-}
 
 //******************************************************************************
 //The yaw is converted to degrees based on the number of slots on the encoder
 // disc (can be adapted to other encoder discs). STATES_ON_DISC is = no. of
 // states (4) * slots on the disc (112).
 //******************************************************************************
-int yawToDeg ()
+int yawToDeg (int yaw)
 {
 	int deg = 0;
 	deg = ((yaw * DEGREES + (STATES_ON_DISC / 2)) / STATES_ON_DISC);
--- yaw_control.h
+++ yaw_control.h
@@ -10,25 +10,36 @@
 #ifndef YAW_CONTROL_H_
 #define YAW_CONTROL_H_
 
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/adc.h"
+#include "driverlib/pwm.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/debug.h"
+#include "driverlib/uart.h"
+#include "drivers/rit128x96x4.h"
+#include "utils/uartstdio.h"
+#include "utils/ustdlib.h"
+#include "utils/isqrt.h"
+#include "utils/circBuf.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+#define DEGREES 360
+#define STATES_ON_DISC 448
 
 
-/* Determines the yaw movement based on state changes.*/
-void yawCalc (void);
-
-
-
-/*The interrupt handler for the for the pin change interrupt. Note that
-the SysTick counter is decrementing.*/
-void YawChangeIntHandler (void);
-
-
-/* Initialising the Pins and interrupt for the yaw*/
-void initYaw (void);
+/* Determines the yaw movement based on state changes and keeps track of changes as yaw.*/
+void yawCalc (int previousState, int currentState, int yaw);
 
 
 /*The yaw is converted to degrees based on the number of slots on the encoder
  disc (can be adapted to other encoder discs). STATES_ON_DISC is = no. of
  states (4) * slots on the disc (112).*/
-int yawToDeg ();
+int yawToDeg (int yaw);
 
 #endif /* YAW_CONTROL_H_ */
