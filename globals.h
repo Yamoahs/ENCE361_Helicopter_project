2104742b261e23aa1cc8859569bd3ee15251b1dd
Samuel Yamoah <sya57@uclive.ac.nz>
2016-06-07 11:14:47 +1200

    renmaing file

---
+++ globals.h
@@ -0,0 +1,30 @@
+/*
+ * stuff.h
+ *
+ *  Created on: Jun 5, 2016
+ *      Author: sya57
+ */
+
+#ifndef STUFF_H_
+#define STUFF_H_
+
+extern  volatile signed int main_duty;
+extern  volatile signed int tail_duty;
+
+extern int desiredHeight;
+extern int desiredYaw;
+
+extern unsigned long period;
+
+extern volatile signed long yawError;
+extern volatile signed long altError;
+
+extern int initialRead; 	// Initial voltage read to calibrate the minimum height of the helicopter
+extern unsigned long g_ulSampCnt;	// Counter for the interrupts
+
+extern int currentState;
+extern int previousState;
+extern int yaw;
+
+
+#endif /* STUFF_H_ */
