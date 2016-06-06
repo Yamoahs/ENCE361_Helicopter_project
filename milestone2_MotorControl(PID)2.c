e18f58aabbc3ff8edb4a365eb85246949204e4ad
Samuel Yamoah <sya57@uclive.ac.nz>
2016-06-06 17:35:15 +1200

    Modifiied main program to work with Altitude Module

--- milestone2_MotorControl(PID)2.c
+++ milestone2_MotorControl(PID)2.c
@@ -6,7 +6,7 @@
 //		***  Version 2 - Calculates Reference based on newHght ***
 //
 // Author:  Samuel Yamoah
-// Date Created: 28.5.2016
+// Date Created: 20.5.2016
 // Last modified: 30.5.2016
 //
 //*****************************************************************************
@@ -35,19 +35,11 @@
 #include "yaw_control.h"
 #include "display.h"
 #include "motor.h"
+#include "altitude.h"
 
-//
 //******************************************************************************
 // Constants
 //******************************************************************************
-
-#define BUF_SIZE 100
-#define SAMPLE_RATE_HZ 10000
-#define MILLI_VOLT 1000
-
-#define ADC_REF 3000
-#define ADC_MAX 1023
-#define ADC_TO_MILLIS(adc) (((adc) * ADC_REF) / ADC_MAX)
 
 #define SYSTICK_RATE_HZ 1000ul
 
@@ -132,12 +124,12 @@ void ButtPressIntHandler (void)
 		}
 
 	if(ulSelect == 0){
-			main_duty = MOTOR_DUTY_MAIN;
-			tail_duty = MOTOR_DUTY_TAIL;
-			PWMOutputState (PWM_BASE, PWM_OUT_1_BIT, true);
-			PWMOutputState (PWM_BASE, PWM_OUT_4_BIT, true);
-			PWMPulseWidthSet (PWM_BASE, PWM_OUT_1, period * main_duty /100);
-			PWMPulseWidthSet (PWM_BASE, PWM_OUT_4, period * tail_duty /100);
+		main_duty = MOTOR_DUTY_MAIN;
+		tail_duty = MOTOR_DUTY_TAIL;
+		PWMOutputState (PWM_BASE, PWM_OUT_1_BIT, true);
+		PWMOutputState (PWM_BASE, PWM_OUT_4_BIT, true);
+		PWMPulseWidthSet (PWM_BASE, PWM_OUT_1, period * main_duty /100);
+		PWMPulseWidthSet (PWM_BASE, PWM_OUT_4, period * tail_duty /100);
 
 	}
 
@@ -211,43 +203,6 @@ void YawChangeIntHandler (void)
 	yawCalc(previousState, currentState, yaw);
 }
 
-//******************************************************************************
-// The handler for the ADC conversion (height) complete interrupt.
-// Writes to the circular buffer.
-//*****************************************************************************
-void HeightIntHandler(void)
-{
-	unsigned long ulValue;
-	static int counter = 0;      //Keeping track of the buffer count for the initialRead
-	int current;
-	int sum = 0;
-	int i;
-
-	// Clean up, clearing the interrupt
-	ADCIntClear(ADC0_BASE, 3);
-
-	// Get the single sample from ADC0. (Yes, I know, I just did what you did sir :p)
-	ADCSequenceDataGet(ADC0_BASE, 3, &ulValue);
-
-	// Place it in the circular buffer (advancing write index)
-	g_inBuffer.data[g_inBuffer.windex] = (int) ulValue;
-	g_inBuffer.windex++;
-	if (g_inBuffer.windex >= g_inBuffer.size)
-		g_inBuffer.windex = 0;
-
-	if (counter < BUF_SIZE) {
-			counter++;
-			if (counter == BUF_SIZE) {
-				for (i = 0; i < BUF_SIZE; i++) {
-					current = ulValue;
-					sum = sum + current;
-				}
-        //Average voltage to calibrate the minimum height of the helicopter
-				initialRead = ADC_TO_MILLIS(sum/BUF_SIZE);
-			}
-		}
-}
-
 //*****************************************************************************
 // Initialisation functions for the clock (incl. SysTick), ADC, display
 //*****************************************************************************
@@ -289,52 +244,6 @@ void initYaw (void)
     // Enable the pin change interrupt
     GPIOPinIntEnable (GPIO_PORTF_BASE, GPIO_PIN_5 | GPIO_PIN_7);
     IntEnable (INT_GPIOF);	// Note: INT_GPIOF defined in inc/hw_ints.h
-}
-
-//******************************************************************************
-void initADC (void)
-{
-  // The ADC0 peripheral must be enabled for configuration and use.
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-
-  // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
-  // will do a single sample when the processor sends a signal to start the
-  // conversion.
-  ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
-
-  // Configure step 0 on sequence 3.  Sample channel 0 (ADC_CTL_CH0) in
-  // single-ended mode (default) and configure the interrupt flag
-  // (ADC_CTL_IE) to be set when the sample is done.  Tell the ADC logic
-  // that this is the last conversion on sequence 3 (ADC_CTL_END).  Sequence
-  // 3 has only one programmable step.  Sequence 1 and 2 have 4 steps, and
-  // sequence 0 has 8 programmable steps.  Since we are only doing a single
-  // conversion using sequence 3 we will only configure step 0.  For more
-  // on the ADC sequences and steps, refer to the LM3S1968 datasheet.
-  ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE |
-                             ADC_CTL_END);
-
-  // Since sample sequence 3 is now configured, it must be enabled.
-  ADCSequenceEnable(ADC0_BASE, 3);
-
-  // Register the interrupt handler
-  ADCIntRegister (ADC0_BASE, 3, HeightIntHandler);
-
-  // Enable interrupts for ADC0 sequence 3 (clears any outstanding interrupts)
-  ADCIntEnable(ADC0_BASE, 3);
-}
-
-
-//******************************************************************************
-//Calculates the current height of the helicopter as a ratio based on the
-// reference and the current height. The returned value is a percentage.
-//******************************************************************************
-int calcHeight(int reference, int current)
-{
-	int height;
-
-	height = (((reference - current)  * 100) / (MILLI_VOLT));
-
-	return height;
 }
 
 
@@ -381,8 +290,6 @@ int main(void)
 			hgt_percent = calcHeight(initialRead, newHght);
 
 		}
-		//PIDControl(hgt_percent, desiredHeight, yaw, desiredYaw, dt, tail_duty, main_duty);
-		//PIDControl(hgt_percent, SysCtlClockGet() / SYSTICK_RATE_HZ);
 		PIDControl(hgt_percent, SysCtlClockGet() / SYSTICK_RATE_HZ, yawError, altError, main_duty, tail_duty, desiredYaw, desiredHeight, yaw);
 
 		PWMPulseWidthSet (PWM_BASE, PWM_OUT_1, period * main_duty / 100);
